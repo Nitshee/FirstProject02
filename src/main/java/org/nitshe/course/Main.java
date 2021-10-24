c7d1a1d4fbf9240ea17455696fae7c1d92605453
Nitshee <piotr7654@gmail.com>
2021-10-24 13:07:55 +0200

    second commit

---
+++ src/main/java/org/nitshe/course/Main.java
@@ -0,0 +1,9 @@
+package org.nitshe.course;
+
+import com.google.common.collect.Sets;
+
+public class Main {
+    public static void main (String[] args){
+        System.out.println("hi");
+    }
+}
